ec102955d2fa61a59b3bb4b52d98eb61b8f9aa2f
Bryan Inacio <bryan.inacio@aluno.senai.br>
2025-02-20 16:52:22 -0300

    Subindo Atividade de API 20/02

---
+++ API_Filmes/api_filmes_senai/Repositories/GeneroRepository.cs
@@ -0,0 +1,73 @@
+﻿using api_filmes_senai.Context;
+using api_filmes_senai.Domains;
+using api_filmes_senai.Interfaces;
+
+namespace api_filmes_senai.Repositories
+{
+    /// <summary>
+    /// Classe que vai implementar a interface IGeneroRepository
+    /// Ou seja vamos implementar os métodos, toda a lógica dos métodos
+    /// </summary>
+    public class GeneroRepository : IGeneroRepository
+    {
+        private readonly Filmes_Context _context;
+
+        /// <summary>
+        /// Contrutor do repositorio 
+        /// Aqui toda vez que o contrutor for chamado ,
+        /// os dados do contexto estarão disponiveis
+        /// </summary>
+        /// <param name="contexto"></param>
+        public GeneroRepository(Filmes_Context contexto)
+        {
+            _context = contexto;
+        }
+
+
+        public void Atualizar(Guid id, Genero genero)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Genero BuscarPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Genero novoGenero)
+        {
+            try
+            {
+                //Adiciona um novo gênero na tabela Gêneros(BD)
+                _context.Genero.Add(novoGenero);
+
+                //Após o cadastro, salva as alterações(BD)
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Deletar(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Genero> Listar()
+        {
+            try
+            {
+                List<Genero> listaGenero = _context.Genero.ToList();
+                return listaGenero;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
